2455cf7cae8292309455841e7ba982e5a900434b
18321473057 <ycs19930606>
2020-03-01 16:34:13 +0800

    初始化

---
+++ demo2/src/test/java/com/line/study/demo2/Demo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.line.study.demo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
